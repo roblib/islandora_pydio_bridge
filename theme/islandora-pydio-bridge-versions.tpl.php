fb39c1093970bb8552f3dbc2b4adbd2babe8ef45
Paul Pound <ppound@upei.ca>
2015-10-14 13:41:13 -0300

    more events and versions stuff

--- theme/islandora-pydio-bridge-versions.tpl.php
+++ theme/islandora-pydio-bridge-versions.tpl.php
@@ -4,6 +4,9 @@
  * Template file to style output.
  */
 ?>
-
+<h2>Object Status</h2>
+<?php print $object_status_table; ?>
+<h2>Lifecycle Events</h2>
+<?php print $lifecycle_events_table; ?>
 <?php print $obj_dsv_table; ?>
 <?php print $ddi_dsv_table; ?>
